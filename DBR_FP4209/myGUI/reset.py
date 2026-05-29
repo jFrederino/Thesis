c7dcdfca0a0cda9ddcb4bf4077a02a2a84960729
James Usher <jfrederino@gmail.com>
2026-05-29 03:20:34 -0400

    rebase

--- DBR_FP4209/myGUI/reset.py
+++
@@ -1,9 +0,0 @@
-# JAMES USHER 2026
-
-from Spectrometer import DBR_Spectrometer
-
-laser = DBR_Spectrometer()
-laser.connect_to_laser("COM4")
-laser.enable()
-laser.disable()
-laser.disconnect_from_voltmeter()
